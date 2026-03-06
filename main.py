7c727f9228601250dad09ac955611f37404e93b0
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2026-03-06 13:13:51 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import  random
+print(random.randrange(1,100))
+
+for x in "banana":
+    print(x)
+
+print(len("hello"))
+print("free" in "free school")
+print("s" not in "samba")
+y = "hu man"
+c = y.upper()
+print(c)
+print(c.lower())
+print(y.strip())
+print(y.replace("u","x"))
+
+x = 21
+y = 4.32
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+dict = {
+    "name":"Suman",
+    "id":2202014,
+    "faculty":"CSE"
+}
+
+print(dict["name"])
